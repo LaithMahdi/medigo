c76b082e761a0f4530476e9630b59c3142f0e822
LaithMahdi <mahdilaith380@gmail.com>
2025-11-25 16:22:49 +0100

    feat: add new specialty SVG icons and update app image constants
    - Added SVG icons for gastroenterologist, gynecologist, neurologist, ophthalmologist, and otologist specialties.
    - Created a new Dart file for app image constants to manage image paths.
    - Introduced onboarding model for onboarding screens with titles, descriptions, and images.
    - Added a specialty model to represent different medical specialties.
    - Updated pubspec.yaml to include new asset paths for specialty images.

---
+++ lib/core/constant/app_image.dart
@@ -0,0 +1,109 @@
+// ignore_for_file: prefer_single_quotes
+class AppImage {
+  AppImage._();
+  
+  static const String imagesCover = "assets/images/cover.png";
+
+  static const String imagesDoctorDoctor1 = "assets/images/doctor/doctor_1.png";
+
+  static const String imagesDoctorDoctor2 = "assets/images/doctor/doctor_2.png";
+
+  static const String imagesDoctorDoctor3 = "assets/images/doctor/doctor_3.png";
+
+  static const String imagesDoctorDoctor4 = "assets/images/doctor/doctor_4.png";
+
+  static const String imagesDoctorDoctor5 = "assets/images/doctor/doctor_5.png";
+
+  static const String imagesDoctorDoctor6 = "assets/images/doctor/doctor_6.png";
+
+  static const String imagesDoctorDoctor7 = "assets/images/doctor/doctor_7.png";
+
+  static const String imagesIconesAfternoon = "assets/images/icones/afternoon.png";
+
+  static const String imagesIconesBadge = "assets/images/icones/badge.svg";
+
+  static const String imagesIconesCalendar = "assets/images/icones/calendar.svg";
+
+  static const String imagesIconesCall = "assets/images/icones/call.svg";
+
+  static const String imagesIconesChecked = "assets/images/icones/checked.png";
+
+  static const String imagesIconesClock = "assets/images/icones/clock.svg";
+
+  static const String imagesIconesExperience = "assets/images/icones/experience.png";
+
+  static const String imagesIconesEye = "assets/images/icones/eye.svg";
+
+  static const String imagesIconesEyeSlash = "assets/images/icones/eye_slash.svg";
+
+  static const String imagesIconesFacebook = "assets/images/icones/facebook.svg";
+
+  static const String imagesIconesFemale = "assets/images/icones/female.png";
+
+  static const String imagesIconesFilter = "assets/images/icones/filter.svg";
+
+  static const String imagesIconesGoogle = "assets/images/icones/google.svg";
+
+  static const String imagesIconesHeart = "assets/images/icones/heart.svg";
+
+  static const String imagesIconesHeartFilled = "assets/images/icones/heart_filled.svg";
+
+  static const String imagesIconesHome = "assets/images/icones/home.svg";
+
+  static const String imagesIconesHomeVisit = "assets/images/icones/home_visit.png";
+
+  static const String imagesIconesHospital = "assets/images/icones/hospital.png";
+
+  static const String imagesIconesMale = "assets/images/icones/male.png";
+
+  static const String imagesIconesMessaging = "assets/images/icones/messaging.svg";
+
+  static const String imagesIconesMorning = "assets/images/icones/morning.png";
+
+  static const String imagesIconesNight = "assets/images/icones/night.png";
+
+  static const String imagesIconesNotification = "assets/images/icones/notification.svg";
+
+  static const String imagesIconesOnline = "assets/images/icones/online.png";
+
+  static const String imagesIconesSearch = "assets/images/icones/search.svg";
+
+  static const String imagesIconesStar = "assets/images/icones/star.svg";
+
+  static const String imagesIconesStar1 = "assets/images/icones/star_1.png";
+
+  static const String imagesIconesStarOutline = "assets/images/icones/star_outline.svg";
+
+  static const String imagesIconesUser = "assets/images/icones/user.svg";
+
+  static const String imagesIconesUsers = "assets/images/icones/users.svg";
+
+  static const String imagesIconesVideoCall = "assets/images/icones/video_call.svg";
+
+  static const String imagesLogoLogoImage = "assets/images/logo/logo_image.svg";
+
+  static const String imagesLogoLogoText = "assets/images/logo/logo_text.svg";
+
+  static const String imagesOnboard1 = "assets/images/onboard_1.png";
+
+  static const String imagesOnboard2 = "assets/images/onboard_2.png";
+
+  static const String imagesOnboard3 = "assets/images/onboard_3.png";
+
+  static const String imagesSpeciailtyCardiologist = "assets/images/speciailty/cardiologist.svg";
+
+  static const String imagesSpeciailtyDentist = "assets/images/speciailty/dentist.svg";
+
+  static const String imagesSpeciailtyEntSpecialist = "assets/images/speciailty/ent_specialist.svg";
+
+  static const String imagesSpeciailtyGastroenterologist = "assets/images/speciailty/gastroenterologist.svg";
+
+  static const String imagesSpeciailtyGynecologist = "assets/images/speciailty/gynecologist.svg";
+
+  static const String imagesSpeciailtyNeurologist = "assets/images/speciailty/neurologist.svg";
+
+  static const String imagesSpeciailtyOphthalmologist = "assets/images/speciailty/ophthalmologist.svg";
+
+  static const String imagesSpeciailtyOtologist = "assets/images/speciailty/otologist.svg";
+}
+
---
+++ lib/data/model/speciality_model.dart
@@ -0,0 +1,7 @@
+class SpecialityModel {
+  String id;
+  String title;
+  String image;
+
+  SpecialityModel({required this.id, required this.title, required this.image});
+}
