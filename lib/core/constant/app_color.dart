8f68b1eda951452224af93093faad56252b47928
LaithMahdi <mahdilaith380@gmail.com>
2025-11-25 16:46:28 +0100

    feat: add AppColor class with color constants for the application

---
+++ lib/core/constant/app_color.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  AppColor._();
+
+  static const Color primary = Color(0xff226CEB);
+  static const Color secondary = Color(0xff48C5E9);
+  // Linear
+  static const Color linear1 = Color(0xff4E8CF7);
+  static const Color linear2 = Color(0xff1A69F0);
+  // Button
+  static const Color button1 = Color(0xff236DEC);
+  // Blue
+  static const Color blue1 = Color(0xff0957DE);
+  static const Color blue2 = Color(0xff2C67CC);
+  static const Color blue3 = Color(0xff0C419D);
+  static const Color info = Color(0xff2F80ED);
+  // Grey
+  static const Color grey1 = Color(0xff808E92);
+  static const Color grey2 = Color(0xffDADADA);
+  static const Color grey3 = Color(0xffF5F5F5);
+  static const Color grey4 = Color(0xffFBFBFB);
+  // Yellow
+  static const Color yellow = Color(0xffFFBB23);
+  static const Color warning = Color(0xffE2B93B);
+  // Green
+  static const Color green = Color(0xff5FD788);
+  static const Color success = Color(0xff27AE60);
+  // Red
+  static const Color red = Color(0xffFF4848);
+  static const Color error = Color(0xffEB5757);
+  // Black
+  static const Color black = Color(0xff1E252D);
+  // White
+  static const Color white = Color(0xffFFFFFF);
+}
