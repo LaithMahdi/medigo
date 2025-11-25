bea983716afb0915b4762b12638d43d0fd244638
LaithMahdi <mahdilaith380@gmail.com>
2025-11-25 18:55:37 +0100

    fix: rename button1 to button for consistency in color constants

--- lib/core/constant/app_color.dart
+++ lib/core/constant/app_color.dart
@@ -9,7 +9,7 @@ class AppColor {
   static const Color linear1 = Color(0xff4E8CF7);
   static const Color linear2 = Color(0xff1A69F0);
   // Button
-  static const Color button1 = Color(0xff236DEC);
+  static const Color button = Color(0xff236DEC);
   // Blue
   static const Color blue1 = Color(0xff0957DE);
   static const Color blue2 = Color(0xff2C67CC);
