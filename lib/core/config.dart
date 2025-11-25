6da04db4696079b0ec24b09cfe5063501a2b0dc5
LaithMahdi <mahdilaith380@gmail.com>
2025-11-25 16:55:12 +0100

    feat: add Config class for application-wide constants

---
+++ lib/core/config.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Config {
+  Config._();
+
+  static EdgeInsets get defaultPadding => const EdgeInsets.all(24);
+  static String get appName => "Medigo";
+}
